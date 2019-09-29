7844f1a01130493744c50c45bfd9c50ecf3cf875
bryan cochrane <bryan@MacBook-Air.lan>
2019-09-29 21:22:55 +0100

    configure nginx

--- __main__.py
+++ __main__.py
@@ -1,17 +1,40 @@
 import pulumi
 from pulumi_kubernetes.apps.v1 import Deployment
+from pulumi_kubernetes.core.v1 import Service
 
-app_labels = { "app": "nginx" }
+# Minikube does not implement services of type `LoadBalancer`; require the user to specify if we're
+# running on minikube, and if so, create only services of type ClusterIP.
+config = pulumi.Config()
+is_minikube = config.require_bool("isMinikube")
+
+app_name = "nginx"
+app_labels = { "app": app_name }
 
 deployment = Deployment(
-    "nginx",
+    app_name,
     spec={
         "selector": { "match_labels": app_labels },
         "replicas": 1,
         "template": {
             "metadata": { "labels": app_labels },
-            "spec": { "containers": [{ "name": "nginx", "image": "nginx" }] }
+            "spec": { "containers": [{ "name": app_name, "image": "nginx" }] }
         }
     })
 
-pulumi.export("name", deployment.metadata["name"])
+# Allocate an IP to the Deployment.
+frontend = Service(
+    app_name,
+    metadata={
+        "labels": deployment.spec["template"]["metadata"]["labels"],
+    },
+    spec={
+        "type": "ClusterIP" if is_minikube else "LoadBalancer",
+        "ports": [{ "port": 80, "target_port": 80, "protocol": "TCP" }],
+        "selector": app_labels,
+    })
+
+# When "done", this will print the public IP.
+if is_minikube:
+    pulumi.export("ip", frontend.spec.apply(lambda v: v["cluster_ip"] if "cluster_ip" in v else None))
+else:
+    pulumi.export("ip", frontend.status.apply(lambda v: v["load_balancer"]["ingress"][0]["ip"] if "load_balancer" in v else None))
